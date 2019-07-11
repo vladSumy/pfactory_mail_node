a8265d4f877556b780233dc51a424cf943919741
vladSumy <gvvlad@mail.ru>
2019-07-11 10:43:02 +0300

    add env.var

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser')
@@ -8,24 +9,25 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 async function main({name, email, phone, comment}){
+    const { FROM_MAIL, FROM_HOST, FROM_PORT, FROM_PASSWORD, TO_MAIL } = process.env
     // Generate test SMTP service account from ethereal.email
     // Only needed if you don't have a real mail account for testing
     let testAccount = await nodemailer.createTestAccount();
 
     // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
-        host: 'smtp.yandex.ru',
-        port: 587,
+        host: FROM_HOST,
+        port: FROM_PORT,
         auth: {
-            user: 'tocontact@pfactory.ru',
-            pass: 'Deal223423'
+            user: FROM_MAIL,
+            pass: FROM_PASSWORD
         }
     });
 
     // send mail with defined transport object
     let info = await transporter.sendMail({
         from: 'tocontact@pfactory.ru', // sender address
-        to: 'teos.nl@gmail.com', // list of receivers
+        to: TO_MAIL, // list of receivers
         subject: "Заявка ✔", // Subject line
         text: `Данные: ${name}(${phone}), email: ${email}, комментарий: ${comment}`, // plain text body
     });
@@ -45,4 +47,7 @@ app.post('/send_mail', (req, res) => {
 
 })
 
-app.listen(4000, () => console.log('Start server pfactory_mail_node...'));
+app.listen(4000, () => {
+    const { FROM_MAIL, FROM_HOST, FROM_PORT, FROM_PASSWORD, TO_MAIL } = process.env
+    console.log('Start server pfactory_mail_node...', FROM_MAIL, FROM_HOST, FROM_PORT, FROM_PASSWORD, TO_MAIL)
+});
